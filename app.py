049a646458d317bb0711d6881a770057588673f2
mefy-Aruna <71429380+mefy-Aruna@users.noreply.github.com>
2020-10-20 14:07:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import numpy as np
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS, cross_origin
+import pickle
+import json
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+model = pickle.load(open('done.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index1.html')
+
+@app.route('/predict',methods=['POST'])
+@cross_origin()
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    if("text" == "M"):
+        text = 0
+    else:
+        text =1
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+    return render_template('index1.html', prediction_text='The Patient is {}'.format(output))
+    
+if __name__ == "__main__":
+    app.run(debug=True) 
